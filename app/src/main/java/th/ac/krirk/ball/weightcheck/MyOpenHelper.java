8404307270a7d14401bced3e217f5f2733f4ee5f
ball <ballzacup0@gmail.com>
2016-02-02 20:03:06 -0800

    Create MyOpenHelper SuceesFull

--- app/src/main/java/th/ac/krirk/ball/weightcheck/MyOpenHelper.java
+++ app/src/main/java/th/ac/krirk/ball/weightcheck/MyOpenHelper.java
@@ -1,10 +1,36 @@
 package th.ac.krirk.ball.weightcheck;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by admin on 2/2/2559.
  */
-public class MyOpenHelper {
+public class MyOpenHelper extends SQLiteOpenHelper {
 
-    public MyOpenHelper() {
+    // Explicit
+    public static final String database_name = "Weight.db";// ประกาศค่าคงที่ ที่ไม่สามารถแก้ไขได้
+    private static final int database_version = 1;
+    private static final String create_table_weight = "create table weightTABLE (" +
+            "_id integer primary key, " +
+            "Date text, " +
+            "Weight text);";
+
+    public MyOpenHelper(Context context) {
+        super(context, database_name, null, database_version);
+    } // Custructor
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(create_table_weight);
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
     }
 } // Main Class
